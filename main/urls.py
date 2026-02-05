54967cb88228c080f6b07ff566be0ce45a1e05a7
Darren Robinson <darrenandamanda.robinson@gmail.com>
2026-02-06 08:52:36 +1100

    Initial set-up

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("backend.urls")),
     path('', RedirectView.as_view(url='/index.html', permanent=False)),
-
 ]
+
+# from django.urls import path, include
+# from django.views.generic import RedirectView
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('backend.urls')),
+#     path('', RedirectView.as_view(url='https://your-frontend-domain/login', permanent=False)),
+# ]
