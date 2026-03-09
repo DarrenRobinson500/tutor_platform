6cade2f491d0f447be2e8a328f370d9fbe1f3b24
Darren Robinson <darrenandamanda.robinson@gmail.com>
2026-03-09 12:23:59 +1100

    add sms

--- backend/tasks.py
+++ backend/tasks.py
@@ -2,7 +2,7 @@ from celery import shared_task
 from .message import process_sms_jobs
 
 @shared_task
-def run_sms_jobs(self):
+def run_sms_jobs():
     print("RUN_SMS_JOBS: task started")
     try:
         process_sms_jobs()
